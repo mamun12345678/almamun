107b2309af0c939d48f40a7f6eb17350eda90bfa
Md Abdullah Al Mamun <o.md.abdullahalmamun@gmail.com>
2025-04-06 03:44:54 +0100

    Fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import Portfolio from './components/Portfolio';
